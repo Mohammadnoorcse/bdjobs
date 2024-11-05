613a56e5bc07010cc0a039e00c7efa605cd19ade
mohammadnoor <mohammadnoorcse@gmail.com>
2024-11-05 10:04:01 +0600

    working navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from './page/Home';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
   return(
       <>
+      <Navbar/>
        <Home/>
       </>
   );
