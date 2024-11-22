b765caf4aba69220ef1ea0e36530143e8341ed86
mohammadnoor <mohammadnoorcse@gmail.com>
2024-11-22 09:03:26 +0600

    working

--- src/components/job/JobDetails.jsx
+++ src/components/job/JobDetails.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import JobCard from './JobCard';
 const slideValue = [
   {
     id: 1,
@@ -39,26 +41,26 @@ const slideValue = [
 const JobDetails = () => {
   return (
     <div className="w-full h-auto flex justify-center items-center bg-[#EEEEEE]">
-      <div className="content flex flex-row my-4">
+      <div className="content flex sm:flex-row flex-col my-4">
         {/* child-1 */}
-        <div className="w-[120px] h-auto flex flex-col gap-3">
+        <div className="sm:w-[120px] w-full h-auto flex flex-col gap-3">
           {/* item-1 */}
           <div className="w-full text-center bg-[#424242] text-[#fff] p-3 rounded-tl-[4px]">
             <h2 className="text-[14px] font-normal">Active filters</h2>
           </div>
           {/* item-2 */}
-          <div className="w-full flex flex-col bg-[#424242] ">
+          <div className="w-full flex sm:flex-col flex-row bg-[#424242] justify-between ">
             {slideValue.map((value) => (
               <div
                 className="flex justify-center items-center flex-col gap-1 text-[11px] font-[600] text-[#ecf0f1] p-3 border-b border-[#303030] cursor-pointer hover:bg-[#4C4C4C]"
                 key={value.id}
               >
                 <img src={value.img} alt="" className="w-[27px] h-[22px]" />
-                <span>{value.name}</span>
+                <span className="sm:flex hidden">{value.name}</span>
               </div>
             ))}
-            <div className="w-full p-1 flex gap-1  justify-center items-center bg-[#424242] text-[11px] font-[600] text-[#ecf0f1] cursor-pointer hover:bg-[#4C4C4C]">
-              <span>Clear All</span>
+            <div className="w-full p-1 sm:flex hidden gap-1  justify-center items-center bg-[#424242] text-[11px] font-[600] text-[#ecf0f1] cursor-pointer hover:bg-[#4C4C4C]">
+              <span className="sm:flex hidden">Clear All</span>
               <img
                 src="https://jobs.bdjobs.com/images/Clear_Icon.png"
                 alt=""
@@ -78,6 +80,21 @@ const JobDetails = () => {
             <span> Save as Favourite</span>
           </div>
           {/* item-2 */}
+          <div className="w-full h-auto bg-[#FFFFFF] rounded-sm p-4 flex flex-col gap-4">
+            {/* card */}
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+          </div>
         </div>
       </div>
     </div>
