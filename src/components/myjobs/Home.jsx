d8c5c2f82104590caad5393c2c824bc751459514
mohammadnoor <mohammadnoorcse@gmail.com>
2024-12-02 10:09:32 +0600

    working project

--- src/components/myjobs/Home.jsx
+++ src/components/myjobs/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { ImStatsDots } from "react-icons/im";
 import {Link} from "react-router-dom"
 import { FaArrowRight } from "react-icons/fa";
+import { FaUnlockAlt } from "react-icons/fa";
 const Home = () => {
   const [active,setActive] = useState('month');
   return (
@@ -26,18 +27,18 @@ const Home = () => {
           </p>
         </div>
         {/* item-2 */}
-        <div className="border border-[#d1c7cd] rounded-[4px] p-4 flex flex-row justify-between ">
-          <div className="w-[33%] border-r-[0.5px] flex flex-col gap-2 p-2">
+        <div className="border border-[#d1c7cd] rounded-[4px] p-4 flex md:flex-row flex-col justify-between ">
+          <div className="md:w-[33%] w-full md:border-r-[0.5px] flex flex-col gap-2 p-2">
             <h3 className="text-[14px] text-[#000] font-bold">Current Plan</h3>
             <h2 className="text-[14px] text-[green] font-bold">Free</h2>
             <div>
-              <Link className="flex justify-center gap-2 items-center bg-[#008020] text-white w-[70%] p-2 border border-[#008020] rounded-[4px] text-[14px] font-medium">
+              <Link className="flex justify-center gap-2 items-center bg-[#008020] text-white md:w-[70%] w-[30%] p-2 border border-[#008020] rounded-[4px] text-[14px] font-medium">
                 Get Bdjobs Pro
                 <FaArrowRight />
               </Link>
             </div>
           </div>
-          <div className="w-[33%] border-r-[0.5px] flex flex-col gap-2 p-2">
+          <div className="md:w-[33%] w-full md:border-r-[0.5px] flex flex-col gap-2 p-2">
             <h3 className="text-[14px] text-[#000] font-bold">My Points</h3>
             <h2 className="text-[14px] text-[#595959] font-normal flex items-center gap-2">
               Total points :<span className="font-semibold">139</span>
@@ -46,7 +47,7 @@ const Home = () => {
               <div className="absolute w-[50%] h-full bg-[green] top-0 left-0  rounded-l-full"></div>
             </div>
             <div className="flex flex-col gap-2">
-              <Link className="flex justify-center gap-2 items-center bg-[#404040] text-white w-[70%] p-2 border border-[#008020] rounded-[4px] text-[14px] font-medium">
+              <Link className="flex justify-center gap-2 items-center bg-[#404040] text-white md:w-[70%] w-full p-2 border border-[#008020] rounded-[4px] text-[14px] font-medium">
                 Get Bdjobs Pro
                 <FaArrowRight />
               </Link>
@@ -55,13 +56,13 @@ const Home = () => {
               </Link>
             </div>
           </div>
-          <div className="w-[33%]  flex flex-col gap-2 p-2">
+          <div className="md:w-[33%] w-full  flex flex-col gap-2 p-2">
             <h3 className="text-[14px] text-[#000] font-bold">SMS Job Alert</h3>
             <span className="text-[13px] text-[#595959]">
               Buy SMS package to get instant offline job alerts
             </span>
             <div>
-              <Link className="flex justify-center gap-2 items-center bg-[#404040] text-white w-[70%] p-2 border border-[#008020] rounded-[4px] text-[14px] font-medium">
+              <Link className="flex justify-center gap-2 items-center bg-[#404040] text-white md:w-[70%] w-full p-2 border border-[#008020] rounded-[4px] text-[14px] font-medium">
                 Buy SMS
                 <FaArrowRight />
               </Link>
@@ -104,7 +105,66 @@ const Home = () => {
               </button>
             </div>
             <div className="border p-4 flex flex-col gap-2">
-              <h2 className='text-base font-medium'>My Activitie</h2>
+              <h2 className="text-[15px] font-medium">My Activitie</h2>
+              <div className="border rounded-[4px] flex sm:justify-between sm:flex-row flex-col ">
+                <div className="sm:w-[33%] w-full p-2 flex flex-col gap-6 sm:border-r border-b ">
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Online Application <span>0</span>
+                  </h2>
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Emailed CV <span>0</span>
+                  </h2>
+                </div>
+                <div className="sm:w-[33%] w-full p-2 flex flex-col gap-6 sm:border-r border-b ">
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Shortlisted Jobs <span>0</span>
+                  </h2>
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Favorite search <span>0</span>
+                  </h2>
+                </div>
+                <div className="sm:w-[33%] w-full p-2 flex flex-col gap-6  ">
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Following Employer(s) <span>0</span>
+                  </h2>
+                </div>
+              </div>
+              <h2 className="text-[15px] font-medium">Profile View Summary</h2>
+              <div className="flex justify-between items-center flex-wrap">
+                <span className="flex gap-2 items-center flex-wrap text-[12px] text-[#004D13]">
+                  Profile Visibility to Employer(s):
+                  <span className="flex items-center gap-1 font-bold">
+                    <FaUnlockAlt />
+                    Public
+                  </span>
+                  <span className="text-[12px] font-bold text-[#0060DB] underline cursor-pointer">
+                    Change
+                  </span>
+                </span>
+                <span className="text-[13px] font-bold text-[#0060DB] cursor-pointer">
+                  Manage Profile
+                </span>
+              </div>
+              <div className="border rounded-[4px] flex sm:justify-between sm:flex-row flex-col ">
+                <div className="sm:w-[33%] w-full p-2 flex flex-col gap-6 sm:border-r border-b ">
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Bdjobs Profile <span>0</span>
+                  </h2>
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Total Company Viewed <span>0</span>
+                  </h2>
+                </div>
+                <div className="sm:w-[33%] w-full p-2 flex flex-col gap-6 sm:border-r border-b ">
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Personalized CV <span>0</span>
+                  </h2>
+                </div>
+                <div className="sm:w-[33%] w-full p-2 flex flex-col gap-6  ">
+                  <h2 className="flex justify-between text-[13px] font-semibold text-[#4D4D4D]">
+                    Video CV <span>0</span>
+                  </h2>
+                </div>
+              </div>
             </div>
           </div>
         </div>
